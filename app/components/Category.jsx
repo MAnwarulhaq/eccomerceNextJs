ea0a64298d80238d48aea89ba64472cb62131f54
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-11-26 18:29:55 +0500

    update the code

--- app/components/Category.jsx
+++ app/components/Category.jsx
@@ -6,21 +6,34 @@ import { VscGame } from 'react-icons/vsc';
 import { useState } from 'react';
 
 export default function Category() {
-      const category = [
+    const category = [
         { icon: <CiMobile4 />, heading: "Phones" },
         { icon: <CiMonitor />, heading: "Computers" },
         { icon: <IoWatchOutline />, heading: "SmartWatch" },
         { icon: <CiCamera />, heading: "Camera" },
         { icon: <CiHeadphones />, heading: "HeadPhones" },
         { icon: <VscGame />, heading: "Gaming" },
-         { icon: <CiMobile4 />, heading: "Phones" },
-           { icon: <CiCamera />, heading: "Camera" },
+        { icon: <CiMobile4 />, heading: "Phones" },
+        { icon: <CiCamera />, heading: "Camera" },
     ]
 
-    const [startIndex,setStartIndex] = useState(0)
-    const [display,setDisplay] = useState(6)
+    const [startIndex, setStartIndex] = useState(0)
+    const [display, setDisplay] = useState(6)
 
-    const dispalyCategory = category.slice(startIndex,display)
+    const displayCategory = category.slice(startIndex, startIndex+display)
+
+    function nextCat (){
+        if(startIndex+display < category.length){
+            setStartIndex(prev => prev +1)
+        }
+    }
+    function prevCat (){
+        if(startIndex > 0){
+            setStartIndex(prev => prev - 1)
+        }
+    }
+
+
 
     return (
         <div className="w-[90%] mx-auto my-10 space-y-5">
@@ -32,12 +45,12 @@ export default function Category() {
                 <p className='font-semibold text-2xl md:text-3xl'>Browse By Category</p>
                 <div className=' flex justify-center md:justify-end items-center md:items-end gap-3'>
                     <div className='size-8 bg-gray-200 rounded-full flex justify-center items-center cursor-pointer'
-                        // onClick={prevItem}
+                    onClick={prevCat}
                     >
                         <GoArrowLeft className='text-lg' />
                     </div>
                     <div className='size-8 bg-gray-200 rounded-full flex justify-center items-center cursor-pointer'
-                        // onClick={nextItem}
+                    onClick={nextCat}
                     >
                         <GoArrowRight className='text-lg' />
 
@@ -45,9 +58,9 @@ export default function Category() {
                 </div>
 
             </div>
-             <div className='grid grid-cols-1 sm:grid-cols-3 xl:grid-cols-6 gap-5'>
+            <div className='grid grid-cols-1 sm:grid-cols-3 xl:grid-cols-6 gap-5'>
 
-                {dispalyCategory.map((item, index) => (
+                {displayCategory.map((item, index) => (
                     <div key={index} className="border border-gray-300 rounded flex flex-col justify-center items-center w-full   h-40 hover:bg-red-500 hover:text-white  cursor-pointer space-y-5">
                         <div className='text-3xl'> {item.icon}</div>
                         <p>{item.heading}</p>
@@ -62,7 +75,7 @@ export default function Category() {
 
 
             </div>
-             <hr  className='mt-10 text-gray-300'/>
+            <hr className='mt-10 text-gray-300' />
         </div>
     )
 }
